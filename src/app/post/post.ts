189049a1b352e64e86e24b98a883d0cba794fd3d
Luiz Xavier <luiznx2002@gmail.com>
2025-07-02 21:34:29 -0400

    adicionado colunas da tabela de sistemas

--- src/app/post/post.ts
+++ src/app/post/post.ts
@@ -1,4 +1,11 @@
 export interface Post {
+status: any;
+responsavel_teste: any;
+data_teste: string|number|Date;
+detalhes_teste: any;
+resultado: any;
+id_implementacao: any;
+id_teste_sistema: any;
     id: number;
     title: string;
     body: string;
