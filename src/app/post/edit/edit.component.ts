1f064ca55e88634f63fd2da475632736ed0e1fcb
sabrinafrazao <sabrinadasilvafrazao@gmail.com>
2025-07-10 09:38:29 -0400

    Criando o edit do sistema

--- src/app/post/edit/edit.component.ts
+++ src/app/post/edit/edit.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-  
 import { PostService } from '../post.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Post } from '../post';
 import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
-  
+
 @Component({
   selector: 'app-edit',
   standalone: true,
@@ -14,59 +13,60 @@ import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angula
   styleUrl: './edit.component.css'
 })
 export class EditComponent {
-  
+
   id!: number;
   post!: Post;
   form!: FormGroup;
-      
-  /*------------------------------------------
-  --------------------------------------------
-  Created constructor
-  --------------------------------------------
-  --------------------------------------------*/
+
   constructor(
     public postService: PostService,
     private route: ActivatedRoute,
     private router: Router
-  ) { }
-      
-  /**
-   * Write code on Method
-   *
-   * @return response()
-   */
+  ) {}
+
   ngOnInit(): void {
     this.id = this.route.snapshot.params['postId'];
-    this.postService.find(this.id).subscribe((data: Post)=>{
-      this.post = data;
-    }); 
-        
+
+    // Inicia o formulário vazio
     this.form = new FormGroup({
-      title: new FormControl('', [Validators.required]),
-      body: new FormControl('', Validators.required)
+      id_implementacao: new FormControl('', Validators.required),
+      resultado: new FormControl('', Validators.required),
+      detalhes_teste: new FormControl('', Validators.required),
+      data_teste: new FormControl('', Validators.required),
+      responsavel_teste: new FormControl('', Validators.required),
+      status: new FormControl('', Validators.required)
+    });
+
+    // Preenche os dados
+    this.postService.find(this.id).subscribe((data: Post) => {
+      this.post = data;
+
+      this.form.setValue({
+        id_implementacao: data.id_implementacao,
+        resultado: data.resultado,
+        detalhes_teste: data.detalhes_teste,
+        data_teste: this.formatDate(data.data_teste),
+        responsavel_teste: data.responsavel_teste,
+        status: data.status
+      });
     });
   }
-      
-  /**
-   * Write code on Method
-   *
-   * @return response()
-   */
-  get f(){
+
+formatDate(date: string | Date): string {
+  const d = new Date(date);
+  return d.toISOString().split('T')[0]; // Retorna no formato yyyy-MM-dd
+}
+
+  get f() {
     return this.form.controls;
   }
-      
-  /**
-   * Write code on Method
-   *
-   * @return response()
-   */
-  submit(){
-    console.log(this.form.value);
-    this.postService.update(this.id, this.form.value).subscribe((res:any) => {
-         console.log('Post updated successfully!');
-         this.router.navigateByUrl('post/index');
-    })
+
+  submit() {
+    if (this.form.invalid) return;
+
+    this.postService.update(this.id, this.form.value).subscribe(() => {
+      console.log('Post updated successfully!');
+      this.router.navigateByUrl('post/index');
+    });
   }
-  
 }
